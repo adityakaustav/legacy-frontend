aff7068210351f98d89d7901e13db2a622fdfc29
Aditya Kaustav Das <aditya.das@infoviz.co>
2021-04-02 16:42:15 +0530

    testing husky

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import LoginPage from './Pages/LoginPage';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  function addOne() {
+    setNumber(number + 2);
+  }
+  function subOne() {
+    setNumber(number - 1);
+  }
+
   return (
     <div className="App">
-    <LoginPage />
-
+      <button type="button" onClick={addOne}>
+        +
+      </button>
+      <button type="button" onClick={subOne}>
+        -
+      </button>
+      <br />
+      <h2>{number}</h2>
     </div>
   );
 }
