d5dae977a876fe806551bf468e7279d14ad794c3
SUBHRAJIT PATTNAYAK <subhrajitpattnayak@gmail.com>
2021-04-05 12:07:41 +0000

    DONE: Environment Setup

---
+++ src/Assets/Style/globalColors.js
@@ -0,0 +1,91 @@
+// Current palette:
+// (1) currentPrimaryBlue:  Use for primary actions, buttons, text links, for indicating
+//                   progress and representing authentication
+// (2) currentPrimaryDark:  Use for body text and headings
+// (3) currentPrimaryLight: Use for page backgrounds
+
+// -----------------------------Default Brand color-------------------------------------------------------
+
+// Default Dark Colors
+export const defaultPrimaryDark = "#172B4D";
+export const defaultSecondaryDark = "#253858";
+export const defaultTertiaryDark = "#42526E";
+
+// Default Mild Colors
+export const defaultPrimaryMild = "#505F79";
+export const defaultSecondaryMild = "#7A869A";
+export const defaultTertiaryMild = "#A5ADBA";
+
+// Default Light Colors
+export const defaultPrimaryLight = "#FFFFFF";
+export const defaultSecondaryLight = "#F4F5F7";
+export const defaultTertiaryLight = "#DFE1E6";
+
+// Default Red Colors
+export const defaultPrimaryRed = "#DE350B";
+export const defaultSecondaryRed = "#FF8F73";
+export const defaultTertiaryRed = "#FFBDAD";
+
+// Default Yellow Colors
+export const defaultPrimaryYellow = "#FF991F";
+export const defaultSecondaryYellow = "#FFC400";
+export const defaultTertiaryYellow = "#FFF0B3";
+
+// Default Green Colors
+export const defaultPrimaryGreen = "#00875A";
+export const defaultSecondaryGreen = "#57D9A3";
+export const defaultTertiaryGreen = "#ABF5D1";
+
+// Default Blue Colors
+export const defaultPrimaryBlue = "#0052CC";
+export const defaultSecondaryBlue = "#2684FF";
+export const defaultTertiaryBlue = "#B3D4FF";
+
+// Default Teal Colors
+export const defaultPrimaryTeal = "#00A3BF";
+export const defaultSecondaryTeal = "#00C7E6";
+export const defaultTertiaryTeal = "#B3F5FF";
+
+// Default Purple Colors
+export const defaultPrimaryPurple = "#5243AA";
+export const defaultSecondaryPurple = "#8777D9";
+export const defaultTertiaryPurple = "#C0B6F2";
+
+// -------------------------Current Color---------------------------------
+
+// Current Dark Colors
+export const currentPrimaryDark = `${defaultPrimaryDark}`;
+export const currentSecondaryDark = `${defaultSecondaryDark}`;
+export const currentTertiaryDark = `${defaultTertiaryDark}`;
+// Current Mild Colors
+export const currentPrimaryMild = `${defaultPrimaryMild}`;
+export const currentSecondaryMild = `${defaultSecondaryMild}`;
+export const currentTertiaryMild = `${defaultTertiaryMild}`;
+// Current Light Colors
+export const currentPrimaryLight = `${defaultPrimaryLight}`;
+export const currentSecondaryLight = `${defaultSecondaryLight}`;
+export const currentTertiaryLight = `${defaultTertiaryLight}`;
+// Current Red Colors
+export const currentPrimaryRed = `${defaultPrimaryRed}`;
+export const currentSecondaryRed = `${defaultSecondaryRed}`;
+export const currentTertiaryRed = `${defaultTertiaryRed}`;
+// Current Yellow Colors
+export const currentPrimaryYellow = `${defaultPrimaryYellow}`;
+export const currentSecondaryYellow = `${defaultSecondaryYellow}`;
+export const currentTertiaryYellow = `${defaultTertiaryYellow}`;
+// Current Green Colors
+export const currentPrimaryGreen = `${defaultPrimaryGreen}`;
+export const currentSecondaryGreen = `${defaultSecondaryGreen}`;
+export const currentTertiaryGreen = `${defaultTertiaryGreen}`;
+// Current Blue Colors
+export const currentPrimaryBlue = `${defaultPrimaryBlue}`;
+export const currentSecondaryBlue = `${defaultSecondaryBlue}`;
+export const currentTertiaryBlue = `${defaultTertiaryBlue}`;
+// Current Teal Colors
+export const currentPrimaryTeal = `${defaultPrimaryTeal}`;
+export const currentSecondaryTeal = `${defaultSecondaryTeal}`;
+export const currentTertiaryTeal = `${defaultTertiaryTeal}`;
+// Current Purple Colors
+export const currentPrimaryPurple = `${defaultPrimaryPurple}`;
+export const currentSecondaryPurple = `${defaultSecondaryPurple}`;
+export const currentTertiaryPurple = `${defaultTertiaryPurple}`;
---
+++ src/Assets/Style/globalFonts.js
@@ -0,0 +1,16 @@
+//Font Weight Variables
+export const extraLightFont = 200;
+
+export const lightFont = 300;
+
+export const regularFont = 400;
+
+export const mediumFont = 500;
+
+export const semiBoldFont = 600;
+
+export const boldFont = 700;
+
+export const extraBoldFont = 800;
+
+export const blackFont = 900;
